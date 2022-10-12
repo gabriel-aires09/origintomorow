76d91beb985788aaca87596cdbb78846af9b5600
Gabriel Oliveira Aires <90778217+gabriel-aires09@users.noreply.github.com>
2022-10-12 12:33:24 -0300

    Deletar arquivos da branch para gh-pages

--- end/Nova cena.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_RegisterButton_pressed() -> void:
-	get_tree().change_scene("res://interface/register/Register.tscn")
-
-
-func _on_LoginButton_pressed() -> void:
-	get_tree().change_scene("res://interface/login/Login.tscn")
--- end/Superscene.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready():
-	var dialogo = Dialogic.start("Conversa")
-	add_child(dialogo)
